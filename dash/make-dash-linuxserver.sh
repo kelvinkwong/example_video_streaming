9ac003d37c9a25021461f9889d33ff32a9c59462
kelvin wong <kelvinwong@outlook.com.au>
2023-09-06 21:22:45 +1000

    initial commit

--- dash/make-dash-linuxserver.sh
+++ dash/make-dash-linuxserver.sh
@@ -14,11 +14,12 @@ docker run --rm -it \
     -map 0:v: -b:v:2 300k -s:v:2 512x288 -c:v:2 libx265 -profile:v:2 main -level:v:2 3 -tag:v:2 hvc1 \
     -map 0:v: -b:v:3 300k -s:v:3 512x288 -c:v:3 libvpx-vp9 \
     -map 0:a: -b:a:0 128k -c:a:0 aac \
-    -map 0:a: -b:a:1 128k -c:a:1 libfdk_aac -profile:a:1 aac_he \
-    -map 0:a: -b:a:2 128k -c:a:2 libfdk_aac -profile:a:2 aac_he_v2 -ac:a:2 2 \
+    -map 0:a: -b:a:1 64k -c:a:1 libfdk_aac -profile:a:1 aac_he \
+    -map 0:a: -b:a:2 64k -c:a:2 libfdk_aac -profile:a:2 aac_he_v2 -ac:a:2 2 \
     -map 0:a: -b:a:3 320k -c:a:3 ac3 \
     -map 0:a: -b:a:4 320k -c:a:4 eac3 \
+    -map 0:a: -b:a:5 64k -c:a:5 libopus -ac:a:5 2 \
     -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 \
     -b_strategy 0 -use_timeline 1 -use_template 1 \
-    -adaptation_sets "id=0,streams=0 id=1,streams=1 id=2,streams=2 id=3,streams=3 id=4,streams=4 id=5,streams=5 id=6,streams=6 id=7,streams=7 id=8,streams=8" \
+    -adaptation_sets "id=0,streams=0 id=1,streams=1 id=2,streams=2 id=3,streams=3 id=4,streams=4 id=5,streams=5 id=6,streams=6 id=7,streams=7 id=8,streams=8 id=9,streams=9" \
     -f dash /config/output-dash-linuxserver/manifest.mpd
